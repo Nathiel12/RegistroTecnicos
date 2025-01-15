d4f1ac202faddc11147ffbc77363b3f4fb03dd36
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-01-15 20:43:30 +0100

    Reapply "Terminado con el html de el componente "TecnicoCreate" y el codigo."
    This reverts commit 12634f83c3f23623ee8d6a6ad22737bb9bfdad99.

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -1,10 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Components;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+var ConStr = builder.Configuration.GetConnectionString("SqlConStr");
+
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(ConStr));
+
+builder.Services.AddScoped<TecnicosService>();
 
 var app = builder.Build();
 
