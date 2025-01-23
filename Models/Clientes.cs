5d885d948a1aedf26312d366a434a76c8b20c17f
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-01-23 01:38:18 +0100

    Crear modelo de Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required]
+        public DateOnly FechaIngreso { get; set; }
+
+        [Required]
+        public string Nombres { get; set; }
+
+        [Required]
+        public string Direccion {  get; set; }
+
+        [Required]
+        [StringLength(11, MinimumLength = 9, ErrorMessage = "El RNC debe tener entre 9 y 11 caracteres.")]
+        public string Rnc {  get; set; }
+
+        [Required]
+        public double LimiteCredito { get; set; }
+        public int TecnicoId { get; set; }
+        
+    }
+}
