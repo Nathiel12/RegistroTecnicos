250872dbae698048ff76a462a96aca50cd089b76
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-02-03 20:09:39 +0100

    closes #18

--- Services/ClientesService.cs
+++ Services/ClientesService.cs
@@ -31,12 +31,6 @@ namespace RegistroTecnicos.Services
             return await contexto.Clientes.AnyAsync(c => c.Nombres.ToLower() == Nombres.ToLower());
         }
 
-        public async Task<bool> ExisteRnc(string Rnc)
-        {
-            await using var contexto = await DbFactory.CreateDbContextAsync();
-            return await contexto.Clientes.AnyAsync(c => c.Nombres.ToLower() == Rnc.ToLower());
-        }
-
         public async Task<bool> ExisteRNC(string Rnc)
         {
             await using var contexto = await DbFactory.CreateDbContextAsync();
