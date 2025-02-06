fe194fbc1894bd7b9d8f6dea0b8dd3b3b30da9f4
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-02-06 19:21:02 +0100

    Crear modelo Sistemas

---
+++ Models/Sistemas.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Sistemas
+    {
+        [Key]
+        public int SistemaId { get; set; }
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="Este campo es requerido")]
+        public string Complejidad { get; set; }
+    }
+}
